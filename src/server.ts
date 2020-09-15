72a7d4302de3f1e6033fd72b3cdce97d67850792
Claau <claudia.alvarez@polijunior.com.br>
2020-09-15 16:59:52 -0300

    cors problem

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { request } from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express()
 
+app.use(cors())
 app.header("Access-Control-Allow-Origin", "*");
 app.use(express.json())
 app.use(routes)
